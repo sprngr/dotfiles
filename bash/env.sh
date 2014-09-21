4972d04888f2b041666969dbacade0080f9fc690
Michael Springer <mike@sprngr.me>
2014-09-21 16:29:34 -0400

    Updated env.sh

--- bash/env.sh
+++ bash/env.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 # Environment
 
+# Set vim as our default editor in the terminal
 export EDITOR='vim'
+
+# don't duplicate lines or lines starting with space in the history
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length
+HISTSIZE=1000
+HISFILESIZE=2000
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# the pattern "**" used in a pathname expansion context will
+# match all files and zero or more directories and subdirectories
+shopt -s globstar
