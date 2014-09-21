fbb43af8d69b2cc12e277c3f02692febd8da99b6
Michael Springer <mike@sprngr.me>
2014-09-21 16:45:05 -0400

    updated env.sh, adds BASH_VERSION_MAJOR variable, note to add info for brew packages on mac os

--- bash/env.sh
+++ bash/env.sh
@@ -4,6 +4,9 @@
 # Set vim as our default editor in the terminal
 export EDITOR='vim'
 
+# Store the major version for bash because we need it later
+export BASH_VERSION_MAJOR=`echo $BASH_VERSION | awk 'BEGIN { FS = "." } ; { print $1 }'`
+
 # don't duplicate lines or lines starting with space in the history
 HISTCONTROL=ignoreboth
 
@@ -20,4 +23,6 @@ shopt -s checkwinsize
 
 # the pattern "**" used in a pathname expansion context will
 # match all files and zero or more directories and subdirectories
-shopt -s globstar
+if [ $BASH_VERSION_MAJOR -gt 3 ]; then
+	shopt -s globstar
+fi
