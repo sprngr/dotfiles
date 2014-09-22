fda6dcb46e2029bb9334b3aabbd2132c4ddd4af9
Michael Springer <michael.springer@theresumator.com>
2014-09-22 16:11:22 -0400

    Updated bang-functions, really need to split this out to it's own project

--- bash/lib/bang-functions.sh
+++ bash/lib/bang-functions.sh
@@ -8,6 +8,12 @@
 # Sadly I cannot use ! to start a function name ;_;
 # We use : as a prefix instead so we do not accidentally impede upon any existing functions
 
+# Todo:
+# Finish out :bang
+# Add urlencode
+# Contemplate adding a :: function to directly hit ddg.com/q?=!$query
+# Add Bang Functions:
+# a, ng, git, gist, 
 
 # defines what to use to open urls based on OS
 if [ $(uname) == 'Darwin' ]; then
@@ -16,37 +22,74 @@ else
 	alias _browser='xdg-open';
 fi
 
+
+# Documentation Template:
+
+# @name Website Name
+# @url website.url
+# @define what does this do
+# @type what kind of site is this
+# @usage function <required> [optional]
+
+
+# @name DuckDuckGo Bang
+# @url ddg.com/q?=!
+# @define Allows you to use DuckDuckGo any !bang not defined
+# @type utility
+# @usage :: <!bang> [query]
+::(){
+	query=$(echo $@ | tr '[:blank:]' '%20')
+	_browser "https://duckduckgo.com/q?=!$query"
+}
+
 # Todo: function to dump list of available bang functions
 # possbily add some form of documentation listing
+# @name Bang
+# @url none
+# @define Prints out information for all of the avaiable bang functions
+# @type utility
+# @usage :bang
 :bang(){
 	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 	echo $DIR
 }
 
-# @site Google
+# @name Google
 # @url google.com
+# @define Search Google
+# @type search
+# @usage :g [query]
 :g() {
 	query=$(echo $@ | tr '[:blank:]' '%20')
 	_browser "https://encrypted.google.com/search?hl=en&q=$query"
 }
 
-# @site YouTube
+# @name YouTube
 # @url youtube.com
+# @define Search YouTube
+# @type video
+# @usage :yt [query]
 :yt(){
 	query=$(echo $@ | tr '[:blank:]' '+')
 	_browser "https://youtube.com/results?search_query=$query"
 }
 
-# @site GitHub
+# @name GitHub
 # @url github.com
+# @define Search Github
+# @type dev
+# @usage :gh [query]
 :gh(){
 	query=$(echo $@ | tr '[:blank:]' '+')
 	_browser "https://github.com/search?q=$query"
 }
 
-# @site Mozilla Developer Network
+# @name Mozilla Developer Network
 # @url developer.mozilla.org
+# @define Search the Mozilla Developer Network
+# @type dev
+# @usage :mdn [query]
 :mdn(){
 	query=$(echo $@ | tr '[:blank:]' '+')
 	_browser "https://developer.mozilla.org/en-US/search?q=$query"
-}
+} 
