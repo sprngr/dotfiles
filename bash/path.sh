931ca14baf2006de2f5286a2bbb345d12ee2b3bc
Michael Springer <mike@sprngr.me>
2015-06-06 09:45:58 -0400

    forgot i export the entire .bin dir, derp

--- bash/path.sh
+++ bash/path.sh
@@ -18,11 +18,6 @@ if [ -s "/usr/local/heroku/bin" ]; then
 	export PATH=/usr/local/heroku/bin:$PATH
 fi
 
-# Pebble SDK 3.0
-if [ -s "$HOME/.bin/pebble-dev" ]; then
-	export PATH=$HOME/.bin/pebble-dev/PebbleSDK-3.0/bin:$PATH
-fi
-
 if [ -s "$HOME/.rvm/scripts/rvm" ]; then 
 	# RVM
 	export PATH=$HOME/.rvm/bin:$PATH
