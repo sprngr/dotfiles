e9bed3e762831a72292d3159ba195f954ecd7658
Michael Springer <michael.springer@jazzhr.com>
2019-02-11 23:44:51 -0500

    Update path, fix syntax issue on macOS

--- homebrew/path.zsh
+++ homebrew/path.zsh
@@ -1,5 +1,8 @@
 # Sets path for linuxbrew if necessary
-if test "$(expr substr $(uname -s) 1 5)" = "Linux"
+if test "$(uname)" = "Darwin"
 then
-    PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
+    # Do nothing
+elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+then
+  PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
 fi
